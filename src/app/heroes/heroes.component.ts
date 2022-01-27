c6663df390cdcbccbc6c52ce6a5b8e58d337ae98
Iago Mendes <iagobrazmendes@gmail.com>
2022-01-27 18:39:25 -0500

    2. display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+	selector: 'app-heroes',
+	templateUrl: './heroes.component.html',
+	styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Doctor Strange'
-  }
+	heroes = HEROES
+	selectedHero?: Hero
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	onSelect(hero: Hero) {
+		this.selectedHero = hero
+	}
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Hulk'},
+  {id: 12, name: 'Iron Man'},
+  {id: 13, name: 'Spiderman'},
+  {id: 14, name: 'Scarlet Witch'},
+  {id: 15, name: 'Vision'},
+  {id: 16, name: 'Loki'},
+  {id: 17, name: 'Deadpool'},
+  {id: 18, name: 'Peacemaker'},
+  {id: 19, name: 'Batman'},
+  {id: 20, name: 'Superman'}
+]
